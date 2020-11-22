83e9bdbd77f029aa68ef2a814407cc9af2bdff6d
bolladaniele <bolladaniele@gmail.com>
2020-11-22 21:11:44 +0000

    deploy

---
+++ precache-manifest.c21b07fc6d3b79083dccb5fbe8b11887.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e34932f71b13596ded8b44db2942479",
+    "url": "/react_openwhyd_demo/index.html"
+  },
+  {
+    "revision": "33638b529cb69a76cf93",
+    "url": "/react_openwhyd_demo/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "56f7eb56c1432715ded1",
+    "url": "/react_openwhyd_demo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "33638b529cb69a76cf93",
+    "url": "/react_openwhyd_demo/static/js/2.794f11ee.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/react_openwhyd_demo/static/js/2.794f11ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56f7eb56c1432715ded1",
+    "url": "/react_openwhyd_demo/static/js/main.246f2629.chunk.js"
+  },
+  {
+    "revision": "3062dee307b44b41df3b",
+    "url": "/react_openwhyd_demo/static/js/runtime-main.05d7f7fe.js"
+  }
+]);
